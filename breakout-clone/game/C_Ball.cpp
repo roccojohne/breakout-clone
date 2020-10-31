4f4e0d38f930281c8f1aa3abc9c5632973723395
Rocco Johne <rocco.johne@arcor.de>
2020-10-31 13:48:51 +0100

    - collision for tragets - data of the default level inc.

--- breakout-clone/game/C_Ball.cpp
+++ breakout-clone/game/C_Ball.cpp
@@ -11,85 +11,101 @@ C_Ball::C_Ball(C_Game& game)
 
 void C_Ball::update(C_Game& game, double frametime)
 {
-   // if (ball_is_on_paddle)
+
+    if (!ball_is_on_paddle)
     {
-       // float x = ball->getPosition().x;
-       // float y = ball->getPosition().y;
-
-        if (!ball_is_on_paddle)
+        switch (flymode)
         {
-            switch (flymode)
+            case ID_BALL_REFLECT: // defalut ball reflect on collision
             {
-                case ID_BALL_REFLECT: // defalut ball reflect on collision
+                // check collision right windowborder
+                if (ball->getGlobalBounds().left + ball->getGlobalBounds().width >= game.getWindow()->getRenderWindow()->getSize().x)
+                    speedx = -speedx;
+                // check collision with top windowborder
+                if (ball->getGlobalBounds().top <= 0)
+                    speedy = -speedy;
+                // check collision with leftborder
+                if (ball->getGlobalBounds().left <= 0)
+                    speedx = -speedx;
+
+                // check collioson with paddle
+                if (ball->getGlobalBounds().intersects(game.getCurrentState()->getPaddle()->getPos()))
                 {
-                    // check collision right windowborder
-                    if (ball->getGlobalBounds().left + ball->getGlobalBounds().width >= game.getWindow()->getRenderWindow()->getSize().x)
-                        speedx = -speedx;
-                    // check collision with top windowborder
-                    if (ball->getGlobalBounds().top <= 0)
-                        speedy = -speedy;
-                    // check collision with leftborder
-                    if (ball->getGlobalBounds().left <= 0)
-                        speedx = -speedx;
-
-                    // check collioson with paddle
-                    if (ball->getGlobalBounds().intersects(game.getCurrentState()->getPaddle()->getPos()))
-                    {
-                        speedy = -speedy;
-                    }
-
-                    // check colision ball with levelitem
-                    for (auto data : game.getCurrentState()->getLevel()->leveldata)
-                    {
-                        if (ball->getGlobalBounds().intersects(data->rs->getGlobalBounds()) && data->is_visible)
-                        {
-                            // check collison direction
-
-                            data->is_visible = false;
-                        }
-                    }
-
-
-                    ball->setPosition(ball->getPosition().x + (speedx * frametime), ball->getPosition().y - (speedy * frametime));
+                    speedy = -speedy;
                 }
-                break;
-                case ID_BALL_FLY_TROUH: // defalut ball reflect on collision
+
+                // check colision ball with levelitem
+                for (auto data : game.getCurrentState()->getLevel()->leveldata)
                 {
-                    // check collision right windowborder
-                    if (ball->getGlobalBounds().left + ball->getGlobalBounds().width >= game.getWindow()->getRenderWindow()->getSize().x)
-                        speedx = -speedx;
-                    // check collision with top windowborder
-                    if (ball->getGlobalBounds().top <= 0)
-                        speedy = -speedy;
-                    // check collision with leftborder
-                    if (ball->getGlobalBounds().left <= 0)
-                        speedx = -speedx;
-
-                    // check collioson with paddle
-                    if (ball->getGlobalBounds().intersects(game.getCurrentState()->getPaddle()->getPos()))
+                    if (ball->getGlobalBounds().intersects(data->rs->getGlobalBounds()) && data->is_visible)
                     {
-                        speedy = -speedy;
+                        // check collison direction
+                        // collision ball vs traget left side
+                        if (ball->getGlobalBounds().left <= data->rs->getGlobalBounds().left + data->rs->getGlobalBounds().width
+                            && ball->getGlobalBounds().top >= data->rs->getGlobalBounds().top
+                            && ball->getGlobalBounds().top <= data->rs->getGlobalBounds().top + data->rs->getGlobalBounds().height)
+                               speedx = -speedx;
+                        // collision ball vs traget right side
+                        else if (ball->getGlobalBounds().left >= data->rs->getGlobalBounds().left + data->rs->getGlobalBounds().width
+                                && ball->getGlobalBounds().top >= data->rs->getGlobalBounds().top
+                                && ball->getGlobalBounds().top <= data->rs->getGlobalBounds().top + data->rs->getGlobalBounds().height)
+                                   speedx = -speedx;
+                        // collision ball vs traget top side
+                        else if (ball->getGlobalBounds().top <= data->rs->getGlobalBounds().top + data->rs->getGlobalBounds().height
+                                 && ball->getGlobalBounds().left >= data->rs->getGlobalBounds().left
+                                 && ball->getGlobalBounds().left <= data->rs->getGlobalBounds().left + data->rs->getGlobalBounds().width)
+                                   speedy = -speedy;
+                        // collision ball vs traget bottom side
+                        else if (ball->getGlobalBounds().top >= data->rs->getGlobalBounds().top + data->rs->getGlobalBounds().height
+                                 && ball->getGlobalBounds().left >= data->rs->getGlobalBounds().left
+                                 && ball->getGlobalBounds().left <= data->rs->getGlobalBounds().left + data->rs->getGlobalBounds().width)
+                                   speedy = -speedy;
+
+                        data->is_visible = false;
                     }
-
-                    // check colision ball with levelitem
-                    for (auto data : game.getCurrentState()->getLevel()->leveldata)
-                    {
-                        if (ball->getGlobalBounds().intersects(data->rs->getGlobalBounds()) && data->is_visible)
-                        {
-                            //speedy = -speedy;
-                            //speedx = -speedx;
-                            data->is_visible = false;
-                        }
-                    }
-
-
-                    ball->setPosition(ball->getPosition().x + (speedx * frametime), ball->getPosition().y - (speedy * frametime));
                 }
-                break;
+
+
+                ball->setPosition(ball->getPosition().x + (speedx * frametime), ball->getPosition().y - (speedy * frametime));
             }
+            break;
+            case ID_BALL_FLY_TROUH: // defalut ball reflect on collision
+            {
+                // check collision right windowborder
+                if (ball->getGlobalBounds().left + ball->getGlobalBounds().width >= game.getWindow()->getRenderWindow()->getSize().x)
+                    speedx = -speedx;
+                // check collision with top windowborder
+                if (ball->getGlobalBounds().top <= 0)
+                    speedy = -speedy;
+                // check collision with leftborder
+                if (ball->getGlobalBounds().left <= 0)
+                    speedx = -speedx;
+
+                // check collioson with paddle
+                if (ball->getGlobalBounds().intersects(game.getCurrentState()->getPaddle()->getPos()))
+                {
+                    speedy = -speedy;
+                }
+
+                // check colision ball with levelitem
+                for (auto data : game.getCurrentState()->getLevel()->leveldata)
+                {
+                    if (ball->getGlobalBounds().intersects(data->rs->getGlobalBounds()) && data->is_visible)
+                    {
+                        //speedy = -speedy;
+                        //speedx = -speedx;
+                        data->is_visible = false;
+                    }
+                }
+
+
+                ball->setPosition(ball->getPosition().x + (speedx * frametime), ball->getPosition().y - (speedy * frametime));
+            }
+            break;
         }
     }
 }
+
 
 void C_Ball::handleevents(C_Game& game, sf::Event& event)
 {
